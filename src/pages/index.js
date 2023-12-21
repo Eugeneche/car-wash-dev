60fb843f4faaa3fbb229025d783c2733666bccfe
Eugeneche <eugene.bizmail@gmail.com>
2023-12-21 15:01:05 +0100

    Corrected SEO in index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,10 +8,13 @@ import Loyality from "../components/Loyality/Loyality"
 import Gallery from "../components/Gallery/Gallery"
 import Gifts from "../components/Gifts/Gifts"
 import Additions from "../components/Additions/Additions"
+import { LocaleContext } from "../components/layout"
 
 
 
 const IndexPage = () => {
+
+  
 
   return (
     <>
@@ -33,6 +36,13 @@ const IndexPage = () => {
  *
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
-export const Head = () => <Seo title="Home" />
+export const Head = ({ data, pageContext: { locale } }) => {
+
+  return (
+    <Seo 
+      title={locale === "cs" ? `Ruční mytí aut v Praze Alfa Wash. Kompletní mytí zvenku a čištění uvnitř vozu` : `Ручна мийка авто в Празі Alfa Wash. Повне миття зовні та очищення всередині автомобіля`}
+      description={locale === "cs" ? `Alfa Wash - ruční mytí aut v Praze. Čištění interiéru: renovace čalounění, plastů, chromu. Mytí karoserie nejlepší autokosmetikou. Zdarma Náhradní vůz a služba Pick up` : `Alfa Wash - ручне миття авто в Празі. Очищення салону: відновлення обивки, пластику, хрому. Миття кузову найкращою автокосметикою. Безкоштовні Авто на заміну та Pick up сервіс`} />
+  )
+}
 
 export default IndexPage
